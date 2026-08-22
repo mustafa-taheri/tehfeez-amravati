b92d8010db3fe9a5cc8d6eaa44eefe703113c463
Mustafa Taheri <muzimax5252@gmail.com>
2026-08-22 13:06:12 +0530

    Docker file version one added

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,6 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  migrations: {
+    path: "prisma/migrations",
+  },
+
   datasource: {
     url: process.env.DATABASE_URL as string,
   },
